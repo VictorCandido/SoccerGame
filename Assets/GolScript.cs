71a1744d25de35fa95eed3d0887256723952e82e
VictorCandido <victorev@outlook.com>
2019-10-23 00:55:31 -0300

    first commit

---
+++ Assets/GolScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolScript : MonoBehaviour
+{
+    public Transform Hominho1Spawner;
+    public Transform Hominho2Spawner;
+    public Transform TopSpawner;
+
+    public GameObject Hominho1pr;
+    public GameObject Hominho2pr;
+    public GameObject Toppr;
+    // Start is called before the first frame update
+    void OnTriggerEnter2D (Collider2D top)
+    {
+        if (top.gameObject.tag == "Top")
+        {
+            int p1Qtd = GameObject.FindGameObjectsWithTag("Player").Length;
+            for (int i = 0; i < p1Qtd; i++)
+            {
+                Destroy(GameObject.FindGameObjectsWithTag("Player")[i]);
+            }
+
+            int p2Qtd = GameObject.FindGameObjectsWithTag("Player2").Length;
+            for (int i = 0; i < p1Qtd; i++)
+            {
+                Destroy(GameObject.FindGameObjectsWithTag("Player2")[i]);
+            }
+
+            int topQtd = GameObject.FindGameObjectsWithTag("Top").Length;
+            for (int i = 0; i < p1Qtd; i++)
+            {
+                Destroy(GameObject.FindGameObjectsWithTag("Top")[i]);
+            }
+
+            Spawner();
+        }
+    }
+
+    // Update is called once per frame
+    void Spawner ()
+    {
+        Instantiate(Hominho1pr, Hominho1Spawner.position, Hominho1Spawner.rotation);
+        Instantiate(Hominho2pr, Hominho2Spawner.position, Hominho2Spawner.rotation);
+        Instantiate(Toppr, TopSpawner.position, TopSpawner.rotation);
+    }
+}
---
+++ Assets/scr.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr : MonoBehaviour
+{
+    public int speed;
+    public string player;
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player == "1")
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(-speed * Time.deltaTime, 0, 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                rb2d.AddForce(Vector3.up * 300000);   
+            }
+        }
+        
+        if(player == "2")
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(-speed * Time.deltaTime, 0, 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                rb2d.AddForce(Vector3.up * 300000);
+            }
+        }
+        
+    }
+}
